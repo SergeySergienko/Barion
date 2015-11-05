62c874059e8d7820c51eb2201741a9e05fe538b8
Sergey Sergienko <sergse2012@yandex.ru>
2015-11-05 19:59:12 +0300

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,11 +4,7 @@
  */
 public class HelloWorld {
     public static void main(String... args) {
-        int x = 2+3;
+        int x = 2 + 3;
         System.out.print("x = " + x);
-        write(String.valueOf(x));
-    }
-
-    private static void write(String s) {
     }
 }
